e33ed197f7e4a29d2c15ef1e7e418d6573741c9e
王川 <wangchuan7@xiaomi.com>
2023-08-03 11:07:59 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('1111@relay.xiaomi.com')
+    print_hi('fgfddsss@xiaomi.com')
+    print_hi('dfgfs32.b2c.srv')
+    print_hi('')
+    print_hi('git.n.xiaomi.com')
+    print_hi('cas.mioffcie.cn')
+    print_hi('ddsfsdfdsf.miui.srv')
+    print_hi('password')
+    print_hi('secret')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
